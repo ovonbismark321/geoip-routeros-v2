e0a42495d70c02f2631bb4a30a888e1ddf3030d5
ovonbismark321 <ovonbismark321@gmail.com>
2026-07-16 18:11:17 +0300

    Update generate_diff.sh

--- scripts/generate_diff.sh
+++ scripts/generate_diff.sh
@@ -5,6 +5,8 @@
 # Generate DIFF
 # ============================================================
 
+set -Eeuo pipefail
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/common.sh"
 
